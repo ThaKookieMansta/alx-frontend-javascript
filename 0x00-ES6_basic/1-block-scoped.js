c6f0e3d8e47c7908023a1c243280929d8ec60116
Chris Mukirae <chriskagiko@gmail.com>
2024-01-03 17:51:55 +0300

    Corrections no 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false
-  const task2 = true
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line no-unused-expressions
-    task
-    // eslint-disable-next-line no-unused-expressions
-    task2
+    // eslint-disable-next-line
+    task;
+    // eslint-disable-next-line
+    task2;
   }
 
-  return [task, task2]
+  return [task, task2];
 }
