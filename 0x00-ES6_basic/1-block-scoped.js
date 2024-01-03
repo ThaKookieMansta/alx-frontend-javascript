ab407ad2bf8c3358d42853a56ba6afffad49c59f
Chris Mukirae <chriskagiko@gmail.com>
2024-01-03 12:14:33 +0300

    corrections

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      
+      task;
+      task2;
+    }
+    
+    return [task, task2];
   }
-
-  return [task, task2];
-}
