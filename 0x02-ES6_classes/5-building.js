d08e2070257b03a12e9460a356a11ea09dedf20b
Chris Mukirae <chriskagiko@gmail.com>
2024-01-04 15:54:37 +0300

    Correction no 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,30 +1,16 @@
-export default class Building() {
+export default class Building {
   /* eslint-disable */
-  export default class Building() {
-  /* eslint-disable */
-  constructor(sqft) {
-    this._sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    if (
-      this.constructor !== Building
-      && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw Error(
-	'Class extending Building must override evacuationWarningMessage',
-      );
+    constructor(sqft = 0) {
+      if (this.constructor !== Building
+          && typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+  
+      this._sqft = sqft;
     }
-
-    this._sqft = sqft;
+  
+    get sqft() {
+      return this._sqft;
+    }
   }
-
-  // getter
-  get sqft() {
-    return this._sqft;
-  }
-
-  // setter 
-  set sqft(newSqft) {
-    this._sqft = newSqft;
-  }
-}
+  
